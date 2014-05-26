327fdf88c6003fc04b8c29112d5bdeaac43316e1
Ben Wu <wubin28@gmail.com>
2014-05-26 10:29:36 +0800

    Made the dull test to pass.

--- tbc-trivia-java/src/test/java/kata/trivia/GameTest.java
+++ tbc-trivia-java/src/test/java/kata/trivia/GameTest.java
@@ -1,12 +1,15 @@
 package kata.trivia;
 
 
+import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
 
 public class GameTest {
     /**
      * Rigourous Test :-)
      */
+    @Test
     public void testApp() {
         assertEquals(5, 2 + 3);
     }
