aa9ac7e3b1d7200e84ece3823f82cce4eafd7b1e
Ben Wu <wubin28@gmail.com>
2014-05-26 10:26:38 +0800

    Added the original Trivia code Game.java and GameRunner.java with a dull test.

---
+++ tbc-trivia-java/src/test/java/kata/trivia/GameTest.java
@@ -0,0 +1,13 @@
+package kata.trivia;
+
+
+import static org.junit.Assert.assertEquals;
+
+public class GameTest {
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp() {
+        assertEquals(5, 2 + 3);
+    }
+}
