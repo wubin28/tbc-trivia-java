ae3b63a28fb966972cc152e99a4239cbbf8c1a6a
Ben Wu <wubin28@gmail.com>
2014-05-27 19:29:40 +0800

    Added acceptance test the_game_should_be_over_if_a_player_rolls_the_dice_for_7_times_and_answers_the_question_wrongly_for_1_time_followed_by_an_odd_rolling_number_but_then_correctly_for_6_times(). Ran it and it passed.

--- tbc-trivia-java/src/test/java/kata/trivia/GameTest.java
+++ tbc-trivia-java/src/test/java/kata/trivia/GameTest.java
@@ -27,6 +27,27 @@ public class GameTest {
         assertFalse(isGameStillInProgress);
     }
 
-    // TODO-acceptance-test: the game should be over if a player rolls the dice for 7 times and answers the question wrongly for 1 time followed by an odd rolling number but then correctly for 6 times
+    @Test
+    public void the_game_should_be_over_if_a_player_rolls_the_dice_for_7_times_and_answers_the_question_wrongly_for_1_time_followed_by_an_odd_rolling_number_but_then_correctly_for_6_times() {
+
+        // Arrange
+        Game game = new Game();
+        game.add("Chet");
+        boolean isGameStillInProgress = true;
+
+        // Act
+        game.roll(1);
+        game.wrongAnswer();
+        game.roll(1);
+        game.wasCorrectlyAnswered();
+        for (int i = 0; i < 5; i++) {
+            game.roll(1);
+            isGameStillInProgress = game.wasCorrectlyAnswered();
+        }
+
+        // Assert
+        assertFalse(isGameStillInProgress);
+    }
+
     // TODO-acceptance-test: the game should be over if a player rolls the dice for 8 times and answers the question wrongly for 1 time followed by an even rolling number but then correctly for 7 times
 }
