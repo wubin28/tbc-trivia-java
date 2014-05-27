66d6f73f420f70f0b0138ca2c907644817728498
Ben Wu <wubin28@gmail.com>
2014-05-27 23:12:40 +0800

    Finished TODO: The return value of method Game.add() is not used.

--- tbc-trivia-java/src/main/java/kata/trivia/Game.java
+++ tbc-trivia-java/src/main/java/kata/trivia/Game.java
@@ -41,8 +41,7 @@ public class Game {
         }
     }
 
-    // TODO-later: The return value of method Game.add() is not used.
-    public boolean add(String playerName) {
+    public void add(String playerName) {
 
 
         players.add(playerName);
@@ -52,7 +51,6 @@ public class Game {
 
         logger.info(playerName + " was added");
         logger.info("They are player number " + players.size());
-        return true;
     }
 
     private int howManyPlayers() {
