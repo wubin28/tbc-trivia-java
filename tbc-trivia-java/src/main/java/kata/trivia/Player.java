90aac9c17e22ab5e90c8a7ff79b0a273e3468dc5
Ben Wu <wubin28@gmail.com>
2014-05-28 22:06:56 +0800

    Created and implemented method Player.stayInPenaltyBox().

--- tbc-trivia-java/src/main/java/kata/trivia/Player.java
+++ tbc-trivia-java/src/main/java/kata/trivia/Player.java
@@ -56,4 +56,8 @@ public class Player {
     public void getOutOfPenaltyBox() {
         this.isGettingOutOfPenaltyBox = true;
     }
+
+    public void stayInPenaltyBox() {
+        this.isGettingOutOfPenaltyBox = false;
+    }
 }
