f2aecb069d73088c94161c1c48ec5a35333c6b81
Ben Wu <wubin28@gmail.com>
2014-05-28 18:48:14 +0800

    Finished the 4 TODOs for testing the class QuestionMaker.

--- tbc-trivia-java/src/test/java/kata/trivia/GameTest.java
+++ tbc-trivia-java/src/test/java/kata/trivia/GameTest.java
@@ -71,14 +71,55 @@ public class GameTest {
         assertFalse(isGameStillInProgress);
     }
 
-    // TODO-working-on: add two pop questions and could remove the first one
     @Test
     public void add_two_pop_questions_and_could_remove_the_first_one() {
+        // Arrange
+        QuestionMaker questionMaker = new QuestionMaker();
+
+        // Act
+        questionMaker.addPopQuestion("Pop Question 1");
+        questionMaker.addPopQuestion("Pop Question 2");
+
         // Assert
         assertEquals("Pop Question 1", questionMaker.removeFirstPopQuestion());
     }
 
-    // TODO: add two science questions and could remove the first one
-    // TODO: add two sports questions and could remove the first one
-    // TODO: add two rock questions and could remove the first one
+    @Test
+    public void add_two_science_questions_and_could_remove_the_first_one() {
+        // Arrange
+        QuestionMaker questionMaker = new QuestionMaker();
+
+        // Act
+        questionMaker.addScienceQuestion("Science Question 1");
+        questionMaker.addScienceQuestion("Science Question 2");
+
+        // Assert
+        assertEquals("Science Question 1", questionMaker.removeFirstScienceQuestion());
+    }
+
+    @Test
+    public void add_two_sports_questions_and_could_remove_the_first_one() {
+        // Arrange
+        QuestionMaker questionMaker = new QuestionMaker();
+
+        // Act
+        questionMaker.addSportsQuestion("Sports Question 1");
+        questionMaker.addSportsQuestion("Sports Question 2");
+
+        // Assert
+        assertEquals("Sports Question 1", questionMaker.removeFirstSportsQuestion());
+    }
+
+    @Test
+    public void add_two_rock_questions_and_could_remove_the_first_one() {
+        // Arrange
+        QuestionMaker questionMaker = new QuestionMaker();
+
+        // Act
+        questionMaker.addRockQuestion("Rock Question 1");
+        questionMaker.addRockQuestion("Rock Question 2");
+
+        // Assert
+        assertEquals("Rock Question 1", questionMaker.removeFirstRockQuestion());
+    }
 }
