ef9ea75b1066b9ed87edde6998c5a2d3ae2bd077
Ben Wu <wubin28@gmail.com>
2014-05-28 19:32:55 +0800

    Added overridden method toString() to class Player to make logging printing correct player names.

--- tbc-trivia-java/src/main/java/kata/trivia/Player.java
+++ tbc-trivia-java/src/main/java/kata/trivia/Player.java
@@ -4,6 +4,14 @@ package kata.trivia;
  * Created by benwu on 14-5-28.
  */
 public class Player {
+    private String playerName;
+
     public Player(String playerName) {
+        this.playerName = playerName;
+    }
+
+    @Override
+    public String toString() {
+        return this.playerName;
     }
 }
