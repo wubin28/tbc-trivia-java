70a474f9eef68b85ec4ee438ab79d16e0ff35bdb
Ben Wu <wubin28@gmail.com>
2014-05-28 20:58:57 +0800

    Removed field places from class Game.

--- tbc-trivia-java/src/main/java/kata/trivia/Game.java
+++ tbc-trivia-java/src/main/java/kata/trivia/Game.java
@@ -13,7 +13,6 @@ public class Game {
     private ArrayList<Player> players = new ArrayList<Player>();
 
     // TODO-working-on: Move places into class Player
-    private int[] places = new int[6];
 
     // TODO: Move purses into class Player
     private int[] purses = new int[6];
