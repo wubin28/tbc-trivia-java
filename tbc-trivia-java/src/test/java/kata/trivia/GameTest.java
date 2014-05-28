b65dd214b5f046e554966f5db63fec6c92a2646f
Ben Wu <wubin28@gmail.com>
2014-05-29 00:10:02 +0800

    Replaced 'TODO: Write tests for class Player' with 6 TODOs for testing place and category of class Player.

--- tbc-trivia-java/src/test/java/kata/trivia/GameTest.java
+++ tbc-trivia-java/src/test/java/kata/trivia/GameTest.java
@@ -123,5 +123,11 @@ public class GameTest {
         assertEquals("Rock Question 1", questionMaker.removeFirstRockQuestion());
     }
 
-    // TODO: Write tests for class Player
+    // TODO: the place should be 1 if the player moves forward 1 step
+    // TODO: the place should be 0 if the player moves forward 12 steps
+
+    // TODO: the category should be Pop if the player moves 12, 4 or 8 steps
+    // TODO: the category should be Science if the player moves 1, 5 or 9 steps
+    // TODO: the category should be Sports if the player moves 2, 6 or 10 steps
+    // TODO: the category should be Rock if the player moves 3, 7 or 11 steps
 }
