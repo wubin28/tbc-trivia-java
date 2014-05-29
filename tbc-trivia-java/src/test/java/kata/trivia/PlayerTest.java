517938dd3684bbcd1744a887e1c16a590a23c8a6
Ben Wu <wubin28@gmail.com>
2014-05-29 10:14:08 +0800

    Moved initialization code to @Before in test class PlayerTest.

--- tbc-trivia-java/src/test/java/kata/trivia/PlayerTest.java
+++ tbc-trivia-java/src/test/java/kata/trivia/PlayerTest.java
@@ -1,5 +1,6 @@
 package kata.trivia;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -8,11 +9,18 @@ import static org.junit.Assert.assertEquals;
  * Created by benwu on 14-5-29.
  */
 public class PlayerTest {
+
+    private Player player = null;
+    private String category = null;
+
+    @Before
+    public void initialize() {
+        // Arrange
+        player = new Player("Ben");
+    }
+
     @Test
     public void the_place_should_be_1_if_the_player_moves_forward_1_step() {
-        // Arrange
-        Player player = new Player("Ben");
-
         // Act
         player.moveForwardSteps(1);
 
@@ -22,9 +30,6 @@ public class PlayerTest {
 
     @Test
     public void the_place_should_be_0_if_the_player_moves_forward_12_steps() {
-        // Arrange
-        Player player = new Player("Ben");
-
         // Act
         player.moveForwardSteps(12);
 
@@ -35,8 +40,7 @@ public class PlayerTest {
     @Test
     public void the_category_should_be_Pop_if_the_player_is_in_place_0_or_4_or_8() {
         // Arrange
-        Player player = new Player("Ben");
-        String category = "Pop";
+        category = "Pop";
 
         // Act, Assert
         player.moveForwardSteps(12);
@@ -52,8 +56,7 @@ public class PlayerTest {
     @Test
     public void the_category_should_be_Science_if_the_player_is_in_place_1_or_5_or_9() {
         // Arrange
-        Player player = new Player("Ben");
-        String category = "Science";
+        category = "Science";
 
         // Act, Assert
         player.moveForwardSteps(1);
@@ -69,8 +72,7 @@ public class PlayerTest {
     @Test
     public void the_category_should_be_Sports_if_the_player_is_in_place_2_or_6_or_10() {
         // Arrange
-        Player player = new Player("Ben");
-        String category = "Sports";
+        category = "Sports";
 
         // Act, Assert
         player.moveForwardSteps(2);
@@ -86,8 +88,7 @@ public class PlayerTest {
     @Test
     public void the_category_should_be_Rock_if_the_player_is_in_place_3_or_7_or_11() {
         // Arrange
-        Player player = new Player("Ben");
-        String category = "Rock";
+        category = "Rock";
 
         // Act, Assert
         player.moveForwardSteps(3);
