d247ed7836aaeb50bbb3ebf0d7c168349448d707
Ben Wu <wubin28@gmail.com>
2014-05-29 08:26:09 +0800

    Finished TODO: the place should be 0 if the player moves forward 12 steps.

--- tbc-trivia-java/src/test/java/kata/trivia/GameTest.java
+++ tbc-trivia-java/src/test/java/kata/trivia/GameTest.java
@@ -135,9 +135,14 @@ public class GameTest {
         assertEquals(1, player.getPlace());
     }
 
-    // TODO-working-on: the place should be 0 if the player moves forward 12 steps
     @Test
     public void the_place_should_be_0_if_the_player_moves_forward_12_steps() {
+        // Arrange
+        Player player = new Player("Ben");
+
+        // Act
+        player.moveForwardSteps(12);
+
         // Assert
         assertEquals(0, player.getPlace());
     }
