a382ab8f676cd0716d94ccb27df09e0429112cc7
Ben Wu <wubin28@gmail.com>
2014-05-29 10:08:07 +0800

    Moved initialization code to @Before in test class GameTest.

--- tbc-trivia-java/src/test/java/kata/trivia/GameTest.java
+++ tbc-trivia-java/src/test/java/kata/trivia/GameTest.java
@@ -1,5 +1,6 @@
 package kata.trivia;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -7,14 +8,19 @@ import static org.junit.Assert.assertFalse;
 
 public class GameTest {
 
+    private Game game = null;
+    private boolean isGameStillInProgress = true;
+
+    @Before
+    public void initialize() {
+        // Arrange
+        game = new Game();
+        game.add("Chet");
+        isGameStillInProgress = true;
+    }
+
     @Test
     public void the_game_should_be_over_if_a_player_rolls_the_dice_and_answers_each_question_correctly_for_6_times() {
-
-        // Arrange
-        Game game = new Game();
-        game.add("Chet");
-        boolean isGameStillInProgress = true;
-
         // Act
         for (int i = 0; i < 6; i++) {
             game.roll(1);
@@ -27,12 +33,6 @@ public class GameTest {
 
     @Test
     public void the_game_should_be_over_if_a_player_rolls_the_dice_for_7_times_and_answers_the_question_wrongly_for_1_time_followed_by_an_odd_rolling_number_but_then_correctly_for_6_times() {
-
-        // Arrange
-        Game game = new Game();
-        game.add("Chet");
-        boolean isGameStillInProgress = true;
-
         // Act
         game.roll(1);
         game.wrongAnswer();
@@ -49,12 +49,6 @@ public class GameTest {
 
     @Test
     public void the_game_should_be_over_if_a_player_rolls_the_dice_for_8_times_and_answers_the_question_wrongly_for_1_time_followed_by_an_even_rolling_number_but_then_correctly_for_7_times_with_odd_rolling_numbers() {
-
-        // Arrange
-        Game game = new Game();
-        game.add("Chet");
-        boolean isGameStillInProgress = true;
-
         // Act
         game.roll(1);
         game.wrongAnswer();
