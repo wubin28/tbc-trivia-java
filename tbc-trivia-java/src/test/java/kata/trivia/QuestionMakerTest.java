1267f152eb137c302ad8ed315b10ed4dac7d4c8d
Ben Wu <wubin28@gmail.com>
2014-05-29 11:06:21 +0800

    Extracted constants in test class QuestionMakerTest to eliminate duplicate code.

--- tbc-trivia-java/src/test/java/kata/trivia/QuestionMakerTest.java
+++ tbc-trivia-java/src/test/java/kata/trivia/QuestionMakerTest.java
@@ -9,6 +9,10 @@ import static org.junit.Assert.assertEquals;
  * Created by benwu on 14-5-29.
  */
 public class QuestionMakerTest {
+    public static final String POP_QUESTION_1 = "Pop Question 1";
+    public static final String SCIENCE_QUESTION_1 = "Science Question 1";
+    public static final String SPORTS_QUESTION_1 = "Sports Question 1";
+    public static final String ROCK_QUESTION_1 = "Rock Question 1";
     private QuestionMaker questionMaker = null;
 
     @Before
@@ -20,41 +24,42 @@ public class QuestionMakerTest {
     @Test
     public void add_two_pop_questions_and_could_remove_the_first_one() {
         // Act
-        questionMaker.addPopQuestion("Pop Question 1");
+        // TODO: Extract variable
+        questionMaker.addPopQuestion(POP_QUESTION_1);
         questionMaker.addPopQuestion("Pop Question 2");
 
         // Assert
-        assertEquals("Pop Question 1", questionMaker.removeFirstPopQuestion());
+        assertEquals(POP_QUESTION_1, questionMaker.removeFirstPopQuestion());
     }
 
     @Test
     public void add_two_science_questions_and_could_remove_the_first_one() {
         // Act
-        questionMaker.addScienceQuestion("Science Question 1");
+        questionMaker.addScienceQuestion(SCIENCE_QUESTION_1);
         questionMaker.addScienceQuestion("Science Question 2");
 
         // Assert
-        assertEquals("Science Question 1", questionMaker.removeFirstScienceQuestion());
+        assertEquals(SCIENCE_QUESTION_1, questionMaker.removeFirstScienceQuestion());
     }
 
     @Test
     public void add_two_sports_questions_and_could_remove_the_first_one() {
         // Act
-        questionMaker.addSportsQuestion("Sports Question 1");
+        questionMaker.addSportsQuestion(SPORTS_QUESTION_1);
         questionMaker.addSportsQuestion("Sports Question 2");
 
         // Assert
-        assertEquals("Sports Question 1", questionMaker.removeFirstSportsQuestion());
+        assertEquals(SPORTS_QUESTION_1, questionMaker.removeFirstSportsQuestion());
     }
 
     @Test
     public void add_two_rock_questions_and_could_remove_the_first_one() {
         // Act
-        questionMaker.addRockQuestion("Rock Question 1");
+        questionMaker.addRockQuestion(ROCK_QUESTION_1);
         questionMaker.addRockQuestion("Rock Question 2");
 
         // Assert
-        assertEquals("Rock Question 1", questionMaker.removeFirstRockQuestion());
+        assertEquals(ROCK_QUESTION_1, questionMaker.removeFirstRockQuestion());
     }
 
 }
