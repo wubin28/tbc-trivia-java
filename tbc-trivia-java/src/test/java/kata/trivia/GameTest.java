5b3129b14fb058f96af29ecd99996703d1882118
Ben Wu <wubin28@gmail.com>
2014-05-29 09:59:24 +0800

    Created test class QuestionMakerTest and moved all question-related tests into it.

--- tbc-trivia-java/src/test/java/kata/trivia/GameTest.java
+++ tbc-trivia-java/src/test/java/kata/trivia/GameTest.java
@@ -70,58 +70,6 @@ public class GameTest {
     }
 
     @Test
-    public void add_two_pop_questions_and_could_remove_the_first_one() {
-        // Arrange
-        QuestionMaker questionMaker = new QuestionMaker();
-
-        // Act
-        questionMaker.addPopQuestion("Pop Question 1");
-        questionMaker.addPopQuestion("Pop Question 2");
-
-        // Assert
-        assertEquals("Pop Question 1", questionMaker.removeFirstPopQuestion());
-    }
-
-    @Test
-    public void add_two_science_questions_and_could_remove_the_first_one() {
-        // Arrange
-        QuestionMaker questionMaker = new QuestionMaker();
-
-        // Act
-        questionMaker.addScienceQuestion("Science Question 1");
-        questionMaker.addScienceQuestion("Science Question 2");
-
-        // Assert
-        assertEquals("Science Question 1", questionMaker.removeFirstScienceQuestion());
-    }
-
-    @Test
-    public void add_two_sports_questions_and_could_remove_the_first_one() {
-        // Arrange
-        QuestionMaker questionMaker = new QuestionMaker();
-
-        // Act
-        questionMaker.addSportsQuestion("Sports Question 1");
-        questionMaker.addSportsQuestion("Sports Question 2");
-
-        // Assert
-        assertEquals("Sports Question 1", questionMaker.removeFirstSportsQuestion());
-    }
-
-    @Test
-    public void add_two_rock_questions_and_could_remove_the_first_one() {
-        // Arrange
-        QuestionMaker questionMaker = new QuestionMaker();
-
-        // Act
-        questionMaker.addRockQuestion("Rock Question 1");
-        questionMaker.addRockQuestion("Rock Question 2");
-
-        // Assert
-        assertEquals("Rock Question 1", questionMaker.removeFirstRockQuestion());
-    }
-
-    @Test
     public void the_place_should_be_1_if_the_player_moves_forward_1_step() {
         // Arrange
         Player player = new Player("Ben");
