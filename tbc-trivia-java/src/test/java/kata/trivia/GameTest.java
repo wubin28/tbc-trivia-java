3dcc606fba07205b6d4ccdb52c2c7e3b3fde2ba3
Ben Wu <wubin28@gmail.com>
2014-05-29 08:40:05 +0800

    Extracted variable category in a test to remove duplicate code.

--- tbc-trivia-java/src/test/java/kata/trivia/GameTest.java
+++ tbc-trivia-java/src/test/java/kata/trivia/GameTest.java
@@ -151,19 +151,21 @@ public class GameTest {
     public void the_category_should_be_Pop_if_the_player_moves_12_or_4_or_8_steps() {
         // Arrange
         Player player = new Player("Ben");
+        String category = "Pop";
 
         // Act, Assert
         player.moveForwardSteps(12);
-        assertEquals("Pop", player.getCurrentCategory());
+        assertEquals(category, player.getCurrentCategory());
 
         player.moveForwardSteps(4);
-        assertEquals("Pop", player.getCurrentCategory());
+        assertEquals(category, player.getCurrentCategory());
 
         player.moveForwardSteps(8);
-        assertEquals("Pop", player.getCurrentCategory());
+        assertEquals(category, player.getCurrentCategory());
     }
 
     // TODO: the category should be Science if the player moves 1, 5 or 9 steps
+
     // TODO: the category should be Sports if the player moves 2, 6 or 10 steps
     // TODO: the category should be Rock if the player moves 3, 7 or 11 steps
 }
